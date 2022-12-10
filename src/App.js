419dcc08bdc5a6dc18b9377c356b7da3a3381179
Jean809 <jean_carlosrosario709@outlook.com>
2022-12-09 22:12:17 -0500

    added a Todo for the card content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Todo from "./components/Todo";
+
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo />
+    </div>
+  );
 }
 
 export default App;
