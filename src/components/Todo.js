83bc59f5e04c2b00495be65728b606ad1d325032
Jean809 <jean_carlosrosario709@outlook.com>
2022-12-11 10:39:24 -0500

    open modal once delete is clicked

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,23 @@
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
+import { useState } from "react";
+
 function Todo(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function deleteHandler() {
+    setModalIsOpen(true);
+  }
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button onClick={deleteHandler} className="btn">
+          Delete
+        </button>
       </div>
+      {modalIsOpen && <Modal />}
+      {modalIsOpen && <Backdrop />}
     </div>
   );
 }
